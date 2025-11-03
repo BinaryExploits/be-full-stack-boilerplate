4a0901cf21e50bf3eeb70977039f6cf69f688888
Anns Shahbaz <annsshahbaz@gmail.com>
2025-11-03 16:44:36 +0500

    fix prisma

--- apps/api/src/crud/crud.service.ts
+++ apps/api/src/crud/crud.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { Crud } from '../generated/prisma';
+import { Crud } from '@repo/prisma-db';
 
 @Injectable()
 export class CrudService {
