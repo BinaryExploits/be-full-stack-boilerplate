e060b7551cdb259c02394c91506418db3ca24e3d
Anns Shahbaz <annsshahbaz@gmail.com>
2025-11-06 12:01:57 +0500

    misc.

--- packages/utils/core/eslint.config.js
+++ packages/utils/core/eslint.config.js
@@ -1,11 +1,18 @@
+// @ts-check
 import { config as baseConfig } from "@repo/eslint-config/base";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(...baseConfig, {
-  languageOptions: {
-    parserOptions: {
-      projectService: true,
-      tsconfigRootDir: import.meta.dirname,
-    },
+export default tseslint.config(
+  ...baseConfig,
+  {
+    ignores: ["eslint.config.js"],
   },
-});
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  }
+);
--- packages/utils/core/src/logger/index.ts
+++ packages/utils/core/src/logger/index.ts
@@ -1,5 +1,5 @@
+export { LogLevel, parseLogLevel, getLogLevelName } from "./log-level";
 export type { ILogger } from "./logger.interface";
 export { Logger } from "./static-logger";
-export { LogLevel, parseLogLevel, getLogLevelName } from "./log-level";
-export { DefaultLogger } from "./default-logger";
 export { BaseLogger } from "./base-logger";
+export { DefaultLogger } from "./default-logger";
