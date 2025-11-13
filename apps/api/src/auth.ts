169f526c81e6f5aed76a091121ef657a9dc98d49
Anns Shahbaz <annsshahbaz@gmail.com>
2025-11-13 13:16:49 +0500

    trusted origins from env

--- apps/api/src/auth.ts
+++ apps/api/src/auth.ts
@@ -7,13 +7,7 @@ const prisma = new PrismaClient();
 
 export const createBetterAuth = () => {
   return betterAuth({
-    trustedOrigins: [
-      'mobile://', // Mobile deep link scheme
-      'exp://192.168.0.6:8081', // Legacy Expo (if needed)
-      'http://localhost:3000', // Web
-      'http://localhost:8081', // Expo Web
-      'http://10.0.2.2:8081', // Android emulator web
-    ],
+    trustedOrigins: process.env.BETTER_AUTH_TRUSTED_ORIGINS?.split(',') || [],
     basePath: '/api/auth',
     database: prismaAdapter(prisma, {
       provider: 'postgresql',
