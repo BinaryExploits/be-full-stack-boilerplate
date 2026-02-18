b66ec8310cbbb1e42f2846e2730a5187c3c5603e
Anns Shahbaz <annsshahbaz@gmail.com>
2026-02-18 12:18:50 +0500

    updates in generation of repos

--- apps/api/src/modules/global-crud/repositories/prisma/global-crud.prisma-repository.ts
+++ apps/api/src/modules/global-crud/repositories/prisma/global-crud.prisma-repository.ts
@@ -16,13 +16,23 @@ export class GlobalCrudPrismaRepository implements IGlobalCrudPrismaRepository {
   ) {}
 
   protected get delegate(): Prisma.GlobalCrudDelegate {
-    return this.prismaTxHost.tx.globalCrud as Prisma.GlobalCrudDelegate;
+    return this.prismaTxHost.tx.globalCrud;
   }
 
   create(
     args: Prisma.GlobalCrudCreateArgs,
   ): Promise<Prisma.GlobalCrudGetPayload<Prisma.GlobalCrudCreateArgs>> {
     return this.delegate.create(args);
+  }
+
+  createMany(args: Prisma.GlobalCrudCreateManyArgs): Promise<Prisma.BatchPayload> {
+    return this.delegate.createMany(args);
+  }
+
+  findFirst(
+    args?: Prisma.GlobalCrudFindFirstArgs,
+  ): Promise<Prisma.GlobalCrudGetPayload<Prisma.GlobalCrudFindFirstArgs> | null> {
+    return this.delegate.findFirst(args);
   }
 
   findUnique(
@@ -43,9 +53,33 @@ export class GlobalCrudPrismaRepository implements IGlobalCrudPrismaRepository {
     return this.delegate.update(args);
   }
 
+  updateMany(args: Prisma.GlobalCrudUpdateManyArgs): Promise<Prisma.BatchPayload> {
+    return this.delegate.updateMany(args);
+  }
+
+  upsert(
+    args: Prisma.GlobalCrudUpsertArgs,
+  ): Promise<Prisma.GlobalCrudGetPayload<Prisma.GlobalCrudUpsertArgs>> {
+    return this.delegate.upsert(args);
+  }
+
   delete(
     args: Prisma.GlobalCrudDeleteArgs,
   ): Promise<Prisma.GlobalCrudGetPayload<Prisma.GlobalCrudDeleteArgs>> {
     return this.delegate.delete(args);
+  }
+
+  deleteMany(args?: Prisma.GlobalCrudDeleteManyArgs): Promise<Prisma.BatchPayload> {
+    return this.delegate.deleteMany(args);
+  }
+
+  count(args?: Prisma.GlobalCrudCountArgs): Promise<number> {
+    return this.delegate.count(args);
+  }
+
+  aggregate(
+    args: Prisma.GlobalCrudAggregateArgs,
+  ): Promise<Prisma.GetGlobalCrudAggregateType<Prisma.GlobalCrudAggregateArgs>> {
+    return this.delegate.aggregate(args);
   }
 }
