d580ae4b452b829d8c55d57a1ae59cec41a48efc
Anns Shahbaz <annsshahbaz@gmail.com>
2026-02-19 12:36:52 +0500

    fromater

--- apps/web/i18n/formatters.ts
+++ apps/web/i18n/formatters.ts
@@ -2,7 +2,8 @@ import type { FormattersInitializer } from "typesafe-i18n";
 import type { Locales, Formatters } from "./i18n-types";
 
 export const initFormatters: FormattersInitializer<Locales, Formatters> = (
-  locale: Locales,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  _locale: Locales,
 ) => {
   const formatters: Formatters = {
     // add your formatter functions here
